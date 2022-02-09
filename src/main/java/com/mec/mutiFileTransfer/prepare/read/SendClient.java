11b0be49ddc9202f6364ac8ebdc271d73ab31a18
wfh <3274597275@qq.com>
2022-02-09 21:23:09 +0800

    client和server基本完成昨天的flag没有倒掉嘿嘿,明天准备测试,完成之后看到ff

--- src/main/java/com/mec/mutiFileTransfer/prepare/read/SendClient.java
+++ src/main/java/com/mec/mutiFileTransfer/prepare/read/SendClient.java
@@ -1,11 +1,10 @@
 package com.mec.mutiFileTransfer.prepare.read;
 
-import com.mec.mutiFileTransfer.prepare.distribute.DefaultDistribute;
-import com.mec.mutiFileTransfer.prepare.distribute.IResourceStrategyDistribute;
+import com.mec.mutiFileTransfer.prepare.resouce.ResourceFileSectionInfo;
 import com.mec.mutiFileTransfer.prepare.resouce.ResourceStructor;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -15,4 +14,40 @@ import java.net.Socket;
  * @Date 2022/2/8 下午9:40
  */
 public class SendClient{
+    public static final int DEFAULT_PORT = 54188;
+    public static final String DEFAULT_SERVER_IP = "127.0.0.1";
+    private int port;
+    private ResourceFileSectionInfo resourceFileSectionInfo;
+    private ResourceStructor resourceStructor;
+    private String serverIp;
+
+    public SendClient() {
+        this.port = DEFAULT_PORT;
+        this.serverIp = DEFAULT_SERVER_IP;
+    }
+
+    public SendClient(ResourceFileSectionInfo resourceFileSectionInfo,ResourceStructor resourceStructor) {
+        this.resourceFileSectionInfo = resourceFileSectionInfo;
+        this.port = DEFAULT_PORT;
+        this.serverIp = DEFAULT_SERVER_IP;
+    }
+
+    public void setResourceStructor(ResourceStructor resourceStructor) {
+        this.resourceStructor = resourceStructor;
+    }
+
+    public void setResourceFileSectionInfo(ResourceFileSectionInfo resourceFileSectionInfo) {
+        this.resourceFileSectionInfo = resourceFileSectionInfo;
+    }
+
+    public void connectToServer() {
+        try {
+            Socket socket = new Socket(this.serverIp,this.port);
+            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+            FileSender fileSender = new FileSender(resourceFileSectionInfo,resourceStructor,dos);
+            fileSender.send();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+}
 }
